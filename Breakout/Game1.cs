8783a20ad5c96529e7978fb6d3b798b1d182df81
Alfred Johnsson <04ALJOC@skola.lidkoping.se>
2022-04-21 14:09:41 +0200

    Klar?

--- Breakout/Game1.cs
+++ Breakout/Game1.cs
@@ -3,6 +3,9 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
 using System;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
+
 
 namespace Breakout
 {
@@ -20,7 +23,7 @@ namespace Breakout
         static Random slump = new Random();
         int slumpVärde = slump.Next(0, 2);
 
-        //TILL SPELARE
+        //SPELARE
         Texture2D vitRekt;
         Rectangle spelare;
         List<Rectangle> spelarLista = new List<Rectangle>();
@@ -28,10 +31,10 @@ namespace Breakout
         KeyboardState TangBordInnan = Keyboard.GetState();
         int hastighet = 10;
         
-        //TILL BOLL
+        //BOLL
         Texture2D boll;
         Rectangle bollRekt;
-        int bollPosX = skärmBredd / 2;
+        int bollPosX = (skärmBredd / 2) - 20;
         int bollPosY = 950;
         static float bollHastighetXPOS = 10;
         float bollHastighetXNEG = bollHastighetXPOS * -1;
@@ -42,7 +45,7 @@ namespace Breakout
         static bool ärIrörelse = false;
         bool harInteIntersectat = true;
 
-        //TILL BLOCK
+        //BLOCK
         Texture2D block1;
         Texture2D block2;
         Texture2D block3;
@@ -51,15 +54,33 @@ namespace Breakout
         List<Texture2D> blockBild = new List<Texture2D>();
         Texture2D tempBild;
 
-        //TILL MENY
+        //MENY
         int menySida = 0;
-        Texture2D startknapp;
-        Texture2D help;
-        Texture2D meny;
+        Texture2D startknapp, help, meny, resetknapp;
         Rectangle startknappPOS = new Rectangle((skärmBredd / 2) - 150, (skärmHöjd / 2) - 50, 300, 100);
-        Rectangle helpknappPOS = new Rectangle((skärmBredd / 2) - 150, (skärmHöjd / 2) + 150, 300, 150);
+        Rectangle helpknappPOS = new Rectangle((skärmBredd / 2) - 220, (skärmHöjd / 2) + 130, 440, 130);
         Rectangle menyknappPOS = new Rectangle((skärmBredd / 2) - 150, (skärmHöjd / 2) + 400, 300, 150);
+        Rectangle resetknappPOS = new Rectangle((skärmBredd / 2) - 150, (skärmHöjd / 2), 300, 150);
+        Rectangle menyResetknappPOS = new Rectangle((skärmBredd / 2) - 150, (skärmHöjd / 2) + 400, 300, 150);
         MouseState mus;
+        Texture2D background;
+        Rectangle backgroundPos = new Rectangle(0, 0, skärmBredd, skärmHöjd);
+        SoundEffect reset, bounce, gameover;
+        bool gameOver = false;
+
+        //INSTRUKTIONER
+        SpriteFont arialFont;
+        string instruktioner = "Styr paddeln med piltangenterna \n" +
+            "För att få paddeln att röra sig fortare, håll ned spacebar \n" +
+            "Påbörja spelet genom att trycka på 'Start' och sedan på spacebar \n" +
+            "Bollen studsar olika beroende på vart på paddeln den träffar \n" +
+            "Men den kan också få slumpvalda hastigheter\noch riktningar när den träffar blocken\n\n" +
+            "Få så mycket poäng som möjligt genom att\nanvänd paddeln för att få bollen att kollidera med och ta bort alla block";
+        Vector2 instruktionPos = new Vector2(200, 100);
+
+        //POÄNG
+        int poäng = 0;
+        Vector2 poängPos = new Vector2((skärmBredd / 2), 10);
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -93,8 +114,13 @@ namespace Breakout
             }
             //MENY
             startknapp = Content.Load<Texture2D>("startknapp");
-            help = Content.Load<Texture2D>("help");
+            help = Content.Load<Texture2D>("instruktioner");
             meny = Content.Load<Texture2D>("meny");
+            background = Content.Load<Texture2D>("bg");
+            reset = Content.Load<SoundEffect>("resetSound");
+            bounce = Content.Load<SoundEffect>("newBounce");
+            gameover = Content.Load<SoundEffect>("gameoverSound");
+            resetknapp = Content.Load<Texture2D>("resetknapp");
 
             //BOLL
             boll = new Texture2D(GraphicsDevice, 1, 1);
@@ -106,23 +132,9 @@ namespace Breakout
             block2 = Content.Load<Texture2D>("sp_brick_reinforced_blue");
             block3 = Content.Load<Texture2D>("sp_brick_black");
             skapaBlock();
-            for (int i = 0; i < blockLista1.Count; i++)
-            {
-                slumpVärde = slump.Next(0, 3);
-                if (slumpVärde == 0)
-                {
-                    tempBild = block1;
-                }
-                if (slumpVärde == 1)
-                {
-                    tempBild = block2;
-                }
-                if (slumpVärde == 2)
-                {
-                    tempBild = block3;
-                }
-                blockBild.Add(tempBild);
-            }
+
+            //INSTRUKTIONER
+            arialFont = Content.Load<SpriteFont>("arial");
         }
 
         protected override void UnloadContent()
@@ -144,6 +156,8 @@ namespace Breakout
                     mus = Mouse.GetState();
                     if (mus.LeftButton == ButtonState.Pressed && startknappPOS.Contains(mus.Position) == true)
                     {
+                        poäng = 0;
+                        gameOver = false;
                         menySida = 1;
                     }
                     else if (mus.LeftButton == ButtonState.Pressed && helpknappPOS.Contains(mus.Position) == true)
@@ -152,11 +166,13 @@ namespace Breakout
                     }
                     break;
                 case 1:
+                    mus = Mouse.GetState();
                     TangBordInnan = TangBord;
                     TangBord = Keyboard.GetState();
                     movement(TangBord, spelare, hastighet, ref spelarLista);
                     bollMovement(ref ärIrörelse, ref bollRekt, TangBord, TangBordInnan);
                     bollKollision();
+                    resetGame();
                     break;
                 case 2:
                     mus = Mouse.GetState();
@@ -166,7 +182,6 @@ namespace Breakout
                     }
                     break;
             }
-            
 
             base.Update(gameTime);
         }
@@ -177,6 +192,7 @@ namespace Breakout
 
             //--------------------------------------------[DRAW]
             _spriteBatch.Begin();
+            _spriteBatch.Draw(background, backgroundPos, Color.White);
             switch (menySida)
             {
                 case 0:
@@ -185,11 +201,21 @@ namespace Breakout
                     break;
                 case 1:
                     ritaSpelare();
-                    _spriteBatch.Draw(boll, bollRekt, Color.White);
+                    if (gameOver == false)
+                    {
+                        _spriteBatch.Draw(boll, bollRekt, Color.White);
+                    }
+                    _spriteBatch.DrawString(arialFont, poäng.ToString(), poängPos, Color.White);
                     ritaBlock();
+                    if (gameOver)
+                    {
+                        _spriteBatch.Draw(resetknapp, resetknappPOS, Color.White);
+                        _spriteBatch.Draw(meny, menyResetknappPOS, Color.White);
+                    }
                     break;
                 case 2:
                     _spriteBatch.Draw(meny, menyknappPOS, Color.White);
+                    _spriteBatch.DrawString(arialFont, instruktioner, instruktionPos, Color.White);
                     break;
             }
             _spriteBatch.End();
@@ -244,18 +270,26 @@ namespace Breakout
             if (bollRekt.X + 40 > skärmBredd)
             {
                 aktuellBollHastighetX = bollHastighetXNEG;
+                bounce.Play();
             }
             if (bollRekt.X < skärmBredd - skärmBredd)
             {
                 aktuellBollHastighetX = bollHastighetXPOS;
-            }
-            if (bollRekt.Y + 40 > skärmHöjd)
-            {
-                aktuellBollHastighetY = bollHastighetYNEG;
+                bounce.Play();
             }
             if (bollRekt.Y < skärmHöjd - skärmHöjd)
             {
                 aktuellBollHastighetY = bollHastighetYPOS;
+                bounce.Play();
+            }
+            //KOLLISION MED UNDERKANT
+            if (bollRekt.Y + 40 > skärmHöjd)
+            {
+                gameOver = true;
+                aktuellBollHastighetY = 0;
+                aktuellBollHastighetX = 0;
+                gameover.Play();
+                bollRekt.Y = bollPosY;
             }
             //KOLLAR KOLLISION MED VÄNSTRA REKTANGEL
             if (spelarLista[0].Intersects(bollRekt) && harInteIntersectat)
@@ -263,6 +297,7 @@ namespace Breakout
                 harInteIntersectat = false;
                 aktuellBollHastighetY = bollHastighetYNEG;
                 aktuellBollHastighetX = bollHastighetXNEG / 2;
+                bounce.Play();
             }
             //KOLLAR KOLLISION MED MITTERSTA REKTANGEL
             if (spelarLista[1].Intersects(bollRekt) && harInteIntersectat)
@@ -270,6 +305,7 @@ namespace Breakout
                 harInteIntersectat = false;
                 aktuellBollHastighetY = bollHastighetYNEG;
                 aktuellBollHastighetX = 0;
+                bounce.Play();
             }
             //KOLLAR KOLLISION MED HÖGRA REKTANGEL
             if (spelarLista[2].Intersects(bollRekt) && harInteIntersectat)
@@ -277,6 +313,7 @@ namespace Breakout
                 harInteIntersectat = false;
                 aktuellBollHastighetY = bollHastighetYNEG;
                 aktuellBollHastighetX = bollHastighetXPOS / 2;
+                bounce.Play();
             }
             //GÖR SÅ BOLLEN KAN INTERSECTA IGEN
             if (bollRekt.Y + 40 > spelare.Y)
@@ -311,6 +348,13 @@ namespace Breakout
                     }
                     blockLista1.RemoveAt(i);
                     blockBild.RemoveAt(i);
+                    poäng++;
+                    if (blockLista1.Count == 0)
+                    {
+                        reset.Play();
+                        skapaBlock();
+                    }
+                    bounce.Play();
                 }
             }
         }
@@ -326,7 +370,24 @@ namespace Breakout
                 blockPos1.Y = (blockPos1.Y + 50) + 5;
                 blockPos1.X = 15;
             }
-
+            for (int i = 0; i < blockLista1.Count; i++)
+            {
+                slumpVärde = slump.Next(0, 3);
+                if (slumpVärde == 0)
+                {
+                    tempBild = block1;
+                }
+                if (slumpVärde == 1)
+                {
+                    tempBild = block2;
+                }
+                if (slumpVärde == 2)
+                {
+                    tempBild = block3;
+                }
+                blockBild.Add(tempBild);
+            }
+            blockPos1.Y = 200;
         }
         public void ritaBlock()
         {
@@ -363,6 +424,40 @@ namespace Breakout
                 {
                     _spriteBatch.Draw(vitRekt, spelarLista[i], Color.Blue);
                 }
+            }
+        }
+        public void resetGame()
+        {
+            if (mus.LeftButton == ButtonState.Pressed && resetknappPOS.Contains(mus.Position) == true && gameOver == true)
+            {
+                for (int i = 0; i < blockLista1.Count; i++)
+                {
+                    blockLista1.Clear();
+                }
+                skapaBlock();
+                gameOver = false;
+                bollRekt.X = bollPosX;
+                bollRekt.Y = bollPosY;
+                ärIrörelse = false;
+                aktuellBollHastighetX = bollHastighetXPOS;
+                aktuellBollHastighetY = bollHastighetYNEG;
+                poäng = 0;
+            }
+            else if (mus.LeftButton == ButtonState.Pressed && menyResetknappPOS.Contains(mus.Position))
+            {
+                for (int i = 0; i < blockLista1.Count; i++)
+                {
+                    blockLista1.Clear();
+                }
+                skapaBlock();
+                gameOver = false;
+                bollRekt.X = bollPosX;
+                bollRekt.Y = bollPosY;
+                ärIrörelse = false;
+                aktuellBollHastighetX = bollHastighetXPOS;
+                aktuellBollHastighetY = bollHastighetYNEG;
+                poäng = 0;
+                menySida = 0;
             }
         }
     }
